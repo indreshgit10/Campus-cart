6274eeb0c284a8b4c3e12636bc78508dfef6c73c
indreshgit10 <indreshkumar456789@gmail.com>
2026-03-07 11:42:50 +0530

    Day 4: Authentication logic (JWT)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 
+import authRoutes from './routes/authRoutes.js';
+
 // Load Environment Variables
 dotenv.config();
 
@@ -14,6 +16,9 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/users', authRoutes);
 
 // Base Route
 app.get('/', (req, res) => {
